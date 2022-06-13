ef3b695a816176c39c3c1d60259a4574bd0303e2
Simon he <13917107460@163.com>
2022-06-13 20:29:41 +0800

    ✨ feature: quickFind api add set and remove

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 - deepCompare  // 比较2个对象的差异返回不同的属性和具体不同的值
 - deepMerge    // Object.assign的深度拷贝版本，返回合并后传入的第一个对象
 - asyncPool   // limit：控制异步并发执行的数量，tasks：异步任务数组
-- quickFind   // quickFind(array: any[], key: any),返回一个新的实例,在实例中find方法可以根据key查找对应的项,查找效率O(1)
+- quickFind   // quickFind(array: any[], key: any),返回一个新的实例,在实例中find方法可以根据key查找对应的项,查找效率O(1),set更新或新增项,delete删除项
 - quickFilter // quickFilter(array: any[], key: string | number | Array<string | number>, value: string | number | RegExp), 快速模糊查找key名字的项,支持正则匹配
 
 ## 使用方法
--- test/basic.test.ts
+++ test/basic.test.ts
@@ -164,12 +164,74 @@ describe('Test 4', () => {
         id: 2,
       },
     ]
-    expect(quickFind(arr, 'id').find(2)).toMatchInlineSnapshot(`
+    const finder = quickFind(arr, 'id')
+    expect(finder.find(2)).toMatchInlineSnapshot(`
       {
         "age": 29,
         "id": 2,
         "name": "simon2",
       }
+    `)
+    expect(finder.set(2, 'name', 'new value~~')).toMatchInlineSnapshot(`
+      [
+        {
+          "age": 18,
+          "id": 0,
+          "name": "simon",
+        },
+        {
+          "age": 19,
+          "id": 1,
+          "name": "simon1",
+        },
+        {
+          "age": 29,
+          "id": 2,
+          "name": "new value~~",
+        },
+      ]
+    `)
+    expect(finder.set(3, { id: 3, name: 'new value' })).toMatchInlineSnapshot(`
+      [
+        {
+          "age": 18,
+          "id": 0,
+          "name": "simon",
+        },
+        {
+          "age": 19,
+          "id": 1,
+          "name": "simon1",
+        },
+        {
+          "age": 29,
+          "id": 2,
+          "name": "new value~~",
+        },
+        {
+          "id": 3,
+          "name": "new value",
+        },
+      ]
+    `)
+    expect(finder.delete(3)).toMatchInlineSnapshot(`
+      [
+        {
+          "age": 18,
+          "id": 0,
+          "name": "simon",
+        },
+        {
+          "age": 19,
+          "id": 1,
+          "name": "simon1",
+        },
+        {
+          "age": 29,
+          "id": 2,
+          "name": "new value~~",
+        },
+      ]
     `)
   })
 })
