d1b7713a0a07b77db7f16da84a3be3f2fcd6c362
Simon he <13917107460@163.com>
2022-06-16 01:20:28 +0800

    feature: add VFetch

---
+++ src/VFetch.ts
@@ -0,0 +1,101 @@
+import type { BodyType, Cache, Credentials, IFetchUtil, Method, Mode, Redirect, ReturnType } from './types'
+export class VFetch {
+  url: string
+  method: Method = 'GET'
+  headers: Record<string, string> = {}
+  credentials: Credentials = 'omit'
+  body: any
+  timeout = 0
+  returnType: ReturnType = 'json'
+  bodyType: BodyType = 'form'
+  firstThen?: (response: Response) => Response
+  result: Promise<any>
+  cache: Cache = 'default'
+  redirect: Redirect = 'manual'
+  mode: Mode = 'cors'
+  constructor(options: IFetchUtil) {
+    const { url, method, headers, bodyType, params, credentials, returnType, timeout, firstThen, cache, redirect, mode } = options
+    this.url = url
+    this.method = method || this.method
+    this.headers = headers || this.headers
+    this.bodyType = bodyType || this.bodyType
+    this.credentials = credentials || this.credentials
+    this.returnType = returnType || this.returnType
+    this.timeout = timeout || this.timeout
+    this.firstThen = firstThen || this.firstThen
+    this.cache = cache || this.cache
+    this.redirect = redirect || this.redirect
+    this.mode = mode || this.mode
+    if (headers)
+      this.set('headers', headers)
+    if (params) {
+      this.set('body', params)
+      this.url += `?${this.bodyToString}`
+    }
+
+    this.result = this.fetch()
+  }
+
+  set(target: keyof FetchUtil, value: Record<string, string> = {}) {
+    Object.keys(value).forEach((key) => {
+      if (this[target] && !this[target][key])
+        this[target][key] = value[key]
+    })
+  }
+
+  async then(callback: Function) {
+    try {
+      const result = await this.result
+      callback(result)
+    }
+    catch (error: any) {
+      throw new Error(error)
+    }
+  }
+
+  get bodyToString() {
+    return Object.keys(this.body!).reduce((result, key) => result += `${key}=${encodeURI(this.body![key])}&`, '')
+  }
+
+  fetch() {
+    if (this.body && this.method !== 'GET') {
+      if (this.bodyType === 'form') {
+        this.set('headers', { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' })
+        this.body = this.bodyToString
+      }
+      else if (this.bodyType === 'file') {
+        this.body = Object.keys(this.body!).reduce((result, key) => {
+          result.append(key, this.body![key])
+          return result
+        }, new FormData())
+      }
+      else if (this.bodyType === 'json') {
+        this.body = JSON.stringify(this.body)
+      }
+    }
+
+    return Promise.race([
+      fetch(this.url, this),
+      new Promise((resolve, reject) => {
+        setTimeout(() => reject(new Error('request timeout')), this.timeout ? this.timeout : 30 * 1000)
+      }),
+    ]).then(
+      (response: any) => this.firstThen
+        ? this.firstThen(response)
+        : response,
+    ).then(
+      (response: Response) => {
+        if (this.returnType === 'json')
+          return response.json()
+        else if (this.returnType === 'text')
+          return response.text()
+        else if (this.returnType === 'blob')
+          return response.blob()
+        else if (this.returnType === 'formData')
+          return response.formData()
+        else if (this.returnType === 'arrayBuffer')
+          return response.arrayBuffer()
+      },
+    )
+  }
+}
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,22 @@
-export interface Props {
+export type Redirect = 'follow' | 'error' | 'manual'
+export type Cache = 'default' | 'no-store' | 'reload' | 'no-cache' | 'force-cache'
+export type Mode = 'cors' | 'no-cors' | 'same-origin' | 'navigate'
+export type ReturnType = 'formData' | 'text' | 'blob' | 'arrayBuffer' | 'json'
+export type BodyType = 'form' | 'json' | 'file'
+export type Method = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'OPTIONS'
+export type Credentials = 'omit' | 'include' | 'same-origin'
 
+export interface IFetchUtil {
+  url: string
+  method?: Method
+  headers?: Record<string, string>
+  credentials?: Credentials
+  params?: Record<string, string>
+  timeout?: number
+  returnType?: ReturnType
+  bodyType?: BodyType
+  cache?: Cache
+  redirect?: Redirect
+  mode?: Mode
+  firstThen?: (response: Response) => Response
 }
