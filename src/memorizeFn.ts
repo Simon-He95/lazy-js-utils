448ba107b6530daae622bef6880675399169e587
Simon he <13917107460@163.com>
2022-06-20 20:52:19 +0800

    ⚡️

--- src/memorizeFn.ts
+++ src/memorizeFn.ts
@@ -1,5 +1,4 @@
-export function memorizeFn(fn: Function) {
-  const cache: Record<string, Function> = {}
+export function memorizeFn(fn: Function, cache: Record<string, Function> = {}) {
   return function (...args: any[]) {
     const _args = JSON.stringify(args)
     return cache[_args] || (cache[_args] = fn.apply(fn, args))
