b339b863fe15da75a3827e2e2f4f8d371a3d256f
Simon he <13917107460@163.com>
2022-06-21 15:46:43 +0800

    chore: add uuid

---
+++ src/uuid.ts
@@ -0,0 +1,18 @@
+export function uuid(len: number, radix: number) {
+  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+  const uuid = [];
+  radix = radix || chars.length;
+  if (len) {
+    for (let i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix];
+  } else {
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+    uuid[14] = '4';
+    for (let i = 0; i < 36; i++) {
+      if (!uuid[i]) {
+        const r = 0 | Math.random() * 16;
+        uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+      }
+    }
+  }
+  return uuid.join('');
+}
