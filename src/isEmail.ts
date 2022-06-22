99574570a517e7d1e31f002c03d613cff5d3fafd
Simon he <13917107460@163.com>
2022-06-22 12:02:07 +0800

    chore: add isEmail

--- src/isEmail.ts
+++ src/isEmail.ts
@@ -1,3 +1,3 @@
-export function isEmail(s: string): boolean {
-  return /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/.test(s)
+export function isEmail(s: string | number): boolean {
+  return /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/.test(s.toString())
 }
