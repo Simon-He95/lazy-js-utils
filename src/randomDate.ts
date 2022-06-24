5d4845ce383b682c3a441bc69ddb5fafd4390e0c
Simon he <13917107460@163.com>
2022-06-24 17:53:47 +0800

    chore: add randomDate

---
+++ src/randomDate.ts
@@ -0,0 +1,12 @@
+export function randomDate(start: string = '1999/01/1', end?: string) {
+  const date = new Date();
+  const [endY, endM, endD] = (end || '').split('/');
+  const y = +endY || date.getFullYear();
+  const m = +endM || date.getMonth() + 1;
+  const d = +endD || date.getDate();
+  const [startY, startM, startD] = start.split('/');
+  const startDate = new Date(+startY, +startM, +startD).getTime();
+  const endDate = new Date(y, m, d).getTime();
+  const timestamp = startDate + Math.round(Math.random() * (endDate - startDate));
+  return new Date(timestamp)
+}
