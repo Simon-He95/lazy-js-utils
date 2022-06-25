b0006588285b901bb6f219bfd6c5decaae351710
Simon he <13917107460@163.com>
2022-06-25 10:20:45 +0800

    chore: enhance log output

--- src/log.ts
+++ src/log.ts
@@ -1,3 +1,3 @@
-export function log(s: string): void {
-  console.log(s)
+export function log(s: string, color = '#00f', fontSize = 14): void {
+  console.log(`%c ${s}`, `color: ${color}; font-size: ${fontSize}px`)
 }
