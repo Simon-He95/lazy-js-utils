ea2987ca20f328a250c1f7a345b6217b93ff88fd
Simon he <13917107460@163.com>
2022-06-26 10:34:24 +0800

    chore: add unescapeHtml

---
+++ src/unescapeHtml.ts
@@ -0,0 +1,3 @@
+export function unescapeHtml(s: string): string {
+  return s.replace(/&amp;|&lt;|&gt;|&#39;|&quot;/g, tag => ({ '&amp;': '&', '&lt;': '<', '&gt;': '>', '&#39;': '\'', '&quot;': '"' }[tag] || tag))
+}
