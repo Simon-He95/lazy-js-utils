5b524b75395356ca63a93e1f55d4bcbc62442ce2
Simon he <13917107460@163.com>
2022-06-26 11:15:22 +0800

    chore: add getLru

---
+++ src/getLru.ts
@@ -0,0 +1,23 @@
+import type { LRU } from './types'
+export function getLru(max = 50): LRU {
+  return {
+    set(key: string, value: any) {
+      if (this.cache.has(key))
+        this.cache.delete(key)
+      this.cache.set(key, value)
+      if (this.cache.size > this.max)
+        this.cache.delete(this.cache.keys().next().value)
+    },
+    get(key: string) {
+      if (this.cache.has(key)) {
+        const value = this.cache.get(key)
+        this.cache.delete(key)
+        this.cache.set(key, value)
+        return value
+      }
+      return undefined
+    },
+    cache: new Map(),
+    max,
+  }
+}
