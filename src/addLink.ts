fabb6df2448c1e2362ce5eeb288e8a46f0c8466a
Simon he <13917107460@163.com>
2022-07-03 16:49:49 +0800

    ⚡️

--- src/addLink.ts
+++ src/addLink.ts
@@ -1,8 +1,8 @@
-import { createElement } from "./createElement"
+import { createElement } from './createElement'
 export function addLink(str: string) {
   document.head.appendChild(createElement('link', {
     rel: 'stylesheet',
     href: str,
-    type: 'text/css'
+    type: 'text/css',
   }))
 }
--- src/addScript.ts
+++ src/addScript.ts
@@ -1,11 +1,12 @@
+import { createElement } from './createElement'
 export function addScript(src: string) {
   try {
-    const s = document.createElement('script')
-    s.type = 'text/javascript'
-    s.async = true
-    s.src = src
     const t = document.getElementsByTagName('script')[0]
-    t.parentNode?.insertBefore(s, t)
+    t.parentNode?.insertBefore(createElement('script', {
+      type: 'text/javascript',
+      src,
+      async: '',
+    }), t)
   }
   catch (error: any) {
     throw new Error(error)
