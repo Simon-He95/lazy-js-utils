e4a1b0cd3012276ecf3ed85f2cdf2febd41f58fd
Simon he <13917107460@163.com>
2022-07-03 16:48:48 +0800

    chore: add addLink

---
+++ src/addLink.ts
@@ -0,0 +1,8 @@
+import { createElement } from "./createElement"
+export function addLink(str: string) {
+  document.head.appendChild(createElement('link', {
+    rel: 'stylesheet',
+    href: str,
+    type: 'text/css'
+  }))
+}
