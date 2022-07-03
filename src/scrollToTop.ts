ba2422161f4b207d26ef47f662301c8b71deff89
Simon he <13917107460@163.com>
2022-07-03 09:40:37 +0800

    fix: scrollToTop

--- src/scrollToTop.ts
+++ src/scrollToTop.ts
@@ -6,7 +6,7 @@ export function scrollToTop() {
       const t = document.documentElement.scrollTop || document.body.scrollTop
       if (pre === undefined)
         pre = t
-      if ((pre + 5 < t && t < pre * 8 / 7) || t === 0)
+      if ((pre < t && t < pre * 8 / 7) || t === 0)
         stop()
       window.scrollTo(0, pre = 7 * t / 8)
     }, 0)
