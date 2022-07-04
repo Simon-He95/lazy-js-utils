08c72a1129fa1d8c74298429a80b75bd2488d47a
Simon he <13917107460@163.com>
2022-07-04 12:34:33 +0800

    chore: update

--- src/addEventListener.ts
+++ src/addEventListener.ts
@@ -1,9 +1,9 @@
-export function addEventListener(eventName: string, callback: (...args: any[]) => void, useCapture?: boolean, autoStop?: boolean) {
-  const stop = () => window.removeEventListener(eventName, callback)
+export function addEventListener(eventName: string, callback: (...args: any[]) => void, useCapture?: boolean, autoStop?: boolean): (() => void) {
+  const remove = () => window.removeEventListener(eventName, callback)
   window.addEventListener(eventName, (e: Event) => {
     callback.call(e.target, e)
     if (autoStop)
-      stop()
+      remove()
   }, useCapture)
-  return stop
+  return remove
 }
