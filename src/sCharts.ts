2f1061b6e935cc15bc5dbd56a886f7857bacc939
Simon he <13917107460@163.com>
2022-07-04 13:34:52 +0800

    chore: add sCharts

--- src/sCharts.ts
+++ src/sCharts.ts
@@ -1,27 +1,49 @@
 import * as echarts from 'echarts'
+import type { EChartsOption, XAXisComponentOption, YAXisComponentOption } from 'echarts'
 import { isStr } from './isStr'
+import { addEventListener } from './addEventListener'
 
-export function sCharts(container: string | HTMLElement, options: Record<string, any>) {
-  if (isStr(container)) {
-    container = document.querySelector(container as string) as HTMLElement
-  }
-  if (!container)
-    console.error('container is not found')
-  const charts = echarts.init(container as HTMLElement, options.theme || '', {
-    width: 500,
-    height: 500
+interface SChartsOption extends EChartsOption {
+  theme?: string | object
+  x: XAXisComponentOption | XAXisComponentOption[]
+  y: YAXisComponentOption | YAXisComponentOption[]
+  w?: number
+  h?: number
+}
+
+export function sCharts(container: string | HTMLElement, options: SChartsOption, autoResize: boolean = true): echarts.ECharts {
+  const fragment = document.createDocumentFragment()
+  const { x, y, xAxis, yAxis, w = 100, h = 100 } = options
+
+  const charts = echarts.init(fragment as unknown as HTMLElement, options.theme || '', {
+    width: w,
+    height: h,
   })
-  const { resize, dispose } = charts
   for (const key in options) {
     if (key.startsWith('on'))
-      charts.on(key.slice(2), options[key])
+      charts.on(key.slice(2), options[key] as (...args: any[]) => void)
   }
-  options.xAxis = options.x || {}
-  options.yAxis = options.y || {}
+  options.xAxis = x || xAxis
+  options.yAxis = y || yAxis
   charts.setOption(options)
+  addEventListener(document, 'DOMContentLoaded', () => {
+    if (isStr(container))
+      container = document.querySelector(container as string) as HTMLElement || container
 
-  return {
-    resize,
-    dispose
+    if (isStr(container))
+      throw new Error(`${container} container is not found`);
+    (container as HTMLElement).appendChild(fragment)
+    if (autoResize)
+      resize()
+  }, false, true)
+  if (autoResize)
+    addEventListener(window, 'resize', resize, false)
+  function resize() {
+    const { offsetWidth, offsetHeight } = container as HTMLElement
+    charts.resize({
+      width: offsetWidth,
+      height: offsetHeight,
+    })
   }
+  return charts
 }
