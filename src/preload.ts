2c78c1ba11cd547fe328cab6e6380769477f7a2e
Simon he <13917107460@163.com>
2022-07-05 08:53:45 +0800

    perf: preload with cloneNode

--- src/preload.ts
+++ src/preload.ts
@@ -2,14 +2,14 @@ import { isArray } from './isArray'
 import { createElement } from './createElement'
 
 export function preload(list: string[] | string) {
+  const imageNode = createElement('img') as HTMLImageElement
   if (isArray(list))
-    (list as string[]).forEach(src => createImage(src))
+    (list as string[]).forEach(src => createImage(imageNode, src))
   else
-    createImage(list as string)
+    createImage(imageNode, list as string)
 }
 
-function createImage(src: string) {
-  createElement('img', {
-    src,
-  })
+function createImage(imageNode: HTMLImageElement, src: string) {
+  const image = imageNode.cloneNode() as HTMLImageElement
+  image.src = src
 }
