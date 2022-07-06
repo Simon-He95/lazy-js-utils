1fd879b55c6c17ab9b675a6c3e9dc85505d36cf9
Simon he <13917107460@163.com>
2022-07-06 21:32:17 +0800

    chore: add shareScreen

---
+++ src/shareScreen.ts
@@ -0,0 +1,21 @@
+import { createElement } from './createElement'
+import { isStr } from './isStr'
+
+export function shareScreen(container: string | HTMLElement, callback: (msg: string) => void) {
+  return new Promise((resolve, reject) => {
+    navigator.mediaDevices.getDisplayMedia({ video: true }).then(handleSuccess, reject)
+    function handleSuccess(stream: MediaStream) {
+      const video = createElement('video', {
+        autoplay: '', playsinline: '', muted: '',
+      }) as HTMLVideoElement
+      video.srcObject = stream
+      if (isStr(container))
+        container = document.querySelector(container as string) as HTMLElement || container
+      if (isStr(container))
+        reject(new Error(`${container} container is not a HTMLElement`))
+      resolve('success')
+      stream.getVideoTracks()[0].addEventListener('ended', () => callback('已停止共享'));
+      (container as HTMLElement).appendChild(video)
+    }
+  })
+}
