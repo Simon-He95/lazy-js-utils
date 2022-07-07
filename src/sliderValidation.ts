e52e425ea5b0436912032b71ebad3909bbfda3ac
Simon he <13917107460@163.com>
2022-07-07 14:14:41 +0800

    chore: fix sliderValidation

--- src/sliderValidation.ts
+++ src/sliderValidation.ts
@@ -97,28 +97,28 @@ export async function sliderValidation(url: string, container: HTMLElement | str
     fragment.appendChild(moveCanvas);
     (container as HTMLElement).appendChild(fragment)
   }
+  addStyle(`@keyframes head-shake {
+    0% {
+      transform: translateX(0);
+    }
+    6.5% {
+      transform: translateX(-6px) rotateY(-9deg);
+    }
+    18.5% {
+      transform: translateX(5px) rotateY(7deg);
+    }
+    31.5% {
+      transform: translateX(-3px) rotateY(-5deg);
+    }
+    43.5% {
+      transform: translateX(2px) rotateY(3deg);
+    }
+    50% {
+      transform: translateX(0);
+    }
+  }
+  .animate-head-shake {
+    animation: head-shake 1s ease-in-out 1;
+  }`)
 }
 
-addStyle(`@keyframes head-shake {
-  0% {
-    transform: translateX(0);
-  }
-  6.5% {
-    transform: translateX(-6px) rotateY(-9deg);
-  }
-  18.5% {
-    transform: translateX(5px) rotateY(7deg);
-  }
-  31.5% {
-    transform: translateX(-3px) rotateY(-5deg);
-  }
-  43.5% {
-    transform: translateX(2px) rotateY(3deg);
-  }
-  50% {
-    transform: translateX(0);
-  }
-}
-.animate-head-shake {
-  animation: head-shake 1s ease-in-out 1;
-}`)
