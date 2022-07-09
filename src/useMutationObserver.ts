89d20ce5beb3ee6121c4036a08bb4859fe85047f
Simon he <13917107460@163.com>
2022-07-09 10:26:59 +0800

    feature: useMutationObserver

--- src/useMutationObserver.ts
+++ src/useMutationObserver.ts
@@ -1,11 +1,30 @@
 import { isStr } from './isStr'
 import type { MutationObserverInit } from './types'
 export function useMutationObserver(element: Element | string, callback: (...args: any[]) => void, options: MutationObserverInit = {}) {
-  if (isStr(element))
-    element = document.querySelector(element as string) as Element || element
-  if (isStr(element))
-    throw new Error(`${element} is not a Element`)
-  const mutationObserver = new MutationObserver(callback)
-  mutationObserver.observe(element as Element, options)
-  return () => mutationObserver.disconnect()
+  let mounted = false
+  let stopped = false
+  update()
+  addEventListener('DOMContentLoaded', update)
+  let stop: () => void
+  function update() {
+    if (isStr(element))
+      element = document.querySelector(element as string) as Element || element
+    if (!mounted && isStr(element))
+      return mounted = true
+    else if (isStr(element))
+      throw new Error(`${element} is not a Element`)
+    const mutationObserver = new MutationObserver(callback)
+    mutationObserver.observe(element as Element, options)
+    stop = () => mutationObserver.disconnect()
+    if (stopped) stop()
+  }
+  return () => {
+    if (!stop) {
+      stopped = true
+      return
+    }
+    stop?.()
+  }
 }
+
+
