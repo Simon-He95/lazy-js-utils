8f7dd2701e491b36615b0ebe0b18724d5acf1d4f
Simon he <13917107460@163.com>
2022-07-11 02:01:16 +0800

    chore:update

--- src/jsonExportZip.ts
+++ src/jsonExportZip.ts
@@ -1,21 +1,21 @@
-import { saveAs } from 'file-saver'
-import JSZip from 'jszip'
+// import { saveAs } from 'file-saver'
+// import JSZip from 'jszip'
 
-export interface exportZipOptions {
-  header: string[]
-  data?: any[][]
-  fileName?: string
-}
+// export interface exportZipOptions {
+//   header: string[]
+//   data?: any[][]
+//   fileName?: string
+// }
 
-export function jsonExportZip(options: exportZipOptions) {
-  const { fileName = 'file', header, data = [] } = options
-  const zip = new JSZip()
-  zip.file(`${fileName}.txt`, data.reduce((result, row) => result += `${row.toString()}\r\n`, `${header}\r\n`))
-  zip.generateAsync({
-    type: 'blob',
-  }).then((blob: any) => {
-    saveAs(blob, `${fileName}.zip`)
-  }, () => {
-    throw new Error('exportZip error')
-  })
-}
+// export function jsonExportZip(options: exportZipOptions) {
+//   const { fileName = 'file', header, data = [] } = options
+//   const zip = new JSZip()
+//   zip.file(`${fileName}.txt`, data.reduce((result, row) => result += `${row.toString()}\r\n`, `${header}\r\n`))
+//   zip.generateAsync({
+//     type: 'blob',
+//   }).then((blob: any) => {
+//     saveAs(blob, `${fileName}.zip`)
+//   }, () => {
+//     throw new Error('exportZip error')
+//   })
+// }
