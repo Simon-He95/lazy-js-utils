5de8cc70a7fe893e642480d13bd6ef4a3b3ae01b
Simon he <13917107460@163.com>
2022-07-27 20:52:37 +0800

    chore: add new function

---
+++ src/useIntersectionObserver.ts
@@ -0,0 +1,28 @@
+import { isStr } from './isStr'
+import { addEventListener } from './addEventListener'
+
+export function useIntersectionObserver(element: Element | string, callback: (entries: IntersectionObserverEntry[]) => void, options: IntersectionObserverInit): () => void {
+  let mounted = false
+  let stop = false
+  const ob = new IntersectionObserver(callback, options)
+  update()
+  addEventListener(document, 'DOMContentLoaded', update)
+
+  return () => {
+    if (isStr(element))
+      return stop = true
+    ob.unobserve(element as Element)
+  }
+
+  function update() {
+    if (isStr(element))
+      element = document.querySelector(element as string) as Element || element
+    if (!mounted && isStr(element))
+      return mounted = true
+    if (isStr(element))
+      throw new Error(`${element} is not a element`)
+    ob.observe(element as Element)
+    if (stop)
+      ob.unobserve(element as Element)
+  }
+}
