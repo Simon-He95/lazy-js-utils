858251ba7b2e15cf396f4e18815aaaeaea5ac432
Simon he <13917107460@163.com>
2022-07-28 17:48:08 +0800

    chore: add writeFile

---
+++ src/writeFile.ts
@@ -0,0 +1,18 @@
+import fs from 'fs'
+import { isStr } from './isStr'
+
+export function writeFile(paths: string[] | string, callback: (content: string, index: number) => string) {
+  if (!callback)
+    throw new Error('callback is required')
+  if (isStr(paths))
+    paths = [paths as string];
+  (paths as string[]).forEach(async (relativepath, i) => {
+    const result = callback(await fs.readFileSync('./dist/index.js', 'utf-8'), i)
+    if (!result)
+      throw new Error('callback needs to return string content')
+    fs.writeFile(relativepath, result, (err) => {
+      if (err)
+        throw err
+    })
+  })
+}
