cd90aefbdfc13801d7d943f122ef656f0604fa4d
Simon he <13917107460@163.com>
2022-08-04 11:06:43 +0800

    chore: update

--- src/isPlainObject.ts
+++ src/isPlainObject.ts
@@ -1,5 +1,5 @@
 import { _toString } from './common'
 
-export function isPlainObject(o: any): o is object {
+export function isPlainObject(o: any): o is Record<any, any> {
   return _toString.call(o) === '[object Object]'
 }
