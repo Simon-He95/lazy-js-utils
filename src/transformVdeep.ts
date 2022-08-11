479c7587a3eab6d7408f924fa4b1de749d1aa2f6
Kilig <1775814844@qq.com>
2022-08-11 17:33:14 +0800

    feat: :ambulance: transformVdeep add ::v-deep support

--- src/transformVdeep.ts
+++ src/transformVdeep.ts
@@ -5,7 +5,7 @@ export function transformVdeep() {
     name: 'vite-transform-Vdeep',
     transform(src: any, id: string) {
       if (id.endsWith('.vue')) {
-        const transformData = src.replace(/(?:\/deep\/|>>>|::deep)([\w.,\n\s-]+){$/gms, (e: string, r: string) => `:deep(${r}){`)
+        const transformData = src.replace(/(?:\/deep\/|>>>|::v-deep)([\w.,\n\s-]+){$/gms, (e: string, r: string) => `:deep(${r}){`)
         if (transformData !== src) {
           fs.writeFile(id, transformData, (err) => {
             if (err)
