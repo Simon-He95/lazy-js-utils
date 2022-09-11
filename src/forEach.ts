20db22787c0dd69ffa8618c35c3f9fe52ea6378d
Simon he <13917107460@163.com>
2022-09-11 21:12:11 +0800

    chore: update

--- src/forEach.ts
+++ src/forEach.ts
@@ -1,16 +1,22 @@
-interface Callback {
-  (value: any, index: number, array: any[], aborted: Function): void
+interface ForEachCallback {
+  (value: any, index: number, array: any[]): void
 }
-export function forEach(array: any[], callback: Callback): void {
-  function aborted() {
-    throw new Error('forEach break')
-  }
+export function forEach(array: any[], callback: ForEachCallback): void {
+  let result
   try {
-    array.forEach((...args) => callback(...args, aborted))
+    array.forEach((...args) => {
+      const res = callback(...args)
+      if (res !== undefined) {
+        result = res
+        throw new Error('forEach break')
+      }
+      return res
+    })
   }
   catch (error: any) {
     if (error.message !== 'forEach break')
       throw error
   }
+  return result
 }
 
