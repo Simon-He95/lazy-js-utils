4cb55881dfca11adadae00b067bdb3812b59d53d
Simon he <13917107460@163.com>
2022-09-14 20:24:57 +0800

    chore: 支持多个shell指令传参

--- src/jsShell.ts
+++ src/jsShell.ts
@@ -1,14 +1,18 @@
 import child_process from 'child_process'
-export function jsShell(commander: string) {
+import { isArray } from './isArray'
+export function jsShell(commander: string | string[]) {
+  return isArray(commander)
+    ? commander.map(command => executor(command))
+    : executor(commander)
+}
+function executor(commander: string) {
   const { status, output } = child_process.spawnSync(commander, {
     shell: true,
     stdio: ['inherit', 'pipe', 'inherit'],
     encoding: 'utf8',
   })
   const result = output[1]
-  if (status === 0)
-    return result
-  else
+  if (status !== 0)
     console.log(result)
-  process.exit(status!)
+  return result
 }
