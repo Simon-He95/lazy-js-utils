658469ff1dd46fef2ed4443af9b5d78c95f6184d
Simon He <hejian1@xd.com>
2022-09-14 15:40:33 +0800

    fix: jsShell 错误抛出

--- src/jsShell.ts
+++ src/jsShell.ts
@@ -1,8 +1,14 @@
 import child_process from 'child_process'
 export function jsShell(commander: string) {
-  return child_process.spawnSync(commander, {
+  const { status, output } = child_process.spawnSync(commander, {
     shell: true,
     stdio: ['inherit', 'pipe', 'inherit'],
     encoding: 'utf8',
-  }).output[1]
+  })
+  const result = output[1]
+  if (status === 0)
+    return result
+  else
+    console.log(result)
+  process.exit(status!)
 }
