7112989ea447dc367bb4e7519b62f8d3afe7cf7e
Simon he <13917107460@163.com>
2022-09-16 01:03:27 +0800

    chore: 添加取消阻止后续事件执行

--- src/jsShell.ts
+++ src/jsShell.ts
@@ -1,18 +1,23 @@
 import child_process from 'child_process'
 import { isArray } from './isArray'
-export function jsShell(commander: string | string[]) {
+export function jsShell(commander: string | string[], errorExit?: boolean) {
   return isArray(commander)
     ? commander.map(command => executor(command))
     : executor(commander)
+  function executor(commander: string) {
+    const { status, output } = child_process.spawnSync(commander, {
+      shell: true,
+      stdio: ['inherit', 'pipe', 'inherit'],
+      encoding: 'utf8',
+    })
+    if (status === 130)
+      process.exit(1)
+    const result = output[1]
+    if (status !== 0) {
+      console.log(result)
+      errorExit && process.exit(1)
+    }
+    return result
+  }
 }
-function executor(commander: string) {
-  const { status, output } = child_process.spawnSync(commander, {
-    shell: true,
-    stdio: ['inherit', 'pipe', 'inherit'],
-    encoding: 'utf8',
-  })
-  const result = output[1]
-  if (status !== 0)
-    console.log(result)
-  return result
-}
+
