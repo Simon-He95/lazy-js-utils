49077d53b7d86fd3d3a89c224cc9cf62ff7a48b6
Simon-He95 <674949287@qq.com>
2022-09-24 23:25:26 +0800

    feat: add new funciton

---
+++ src/getImageData.ts
@@ -0,0 +1,29 @@
+import { Canvas } from './Canvas'
+interface ImageData {
+  width: number
+  height: number
+  data: Uint8ClampedArray
+}
+export function getImageData(src: string): Promise<ImageData> {
+  return new Promise((resolve, reject) => {
+    try {
+      const image = new Image()
+      image.src = src
+      image.onload = function () {
+        const { width, height } = image
+        const { ctx } = new Canvas(width, height)
+        ctx.drawImage(image, 0, 0)
+        resolve({
+          width,
+          height,
+          data: ctx.getImageData(0, 0, width, height).data,
+        })
+      }
+      image.onerror = reject
+    }
+    catch (error) {
+      reject(error)
+    }
+  })
+}
+
