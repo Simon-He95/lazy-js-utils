1ef4d4dde1d46ada49fe230a38ed5d0687320014
Simon he <13917107460@163.com>
2022-09-24 23:24:58 +0800

    chore: update

--- src/Canvas.ts
+++ src/Canvas.ts
@@ -1,7 +1,11 @@
 export class Canvas {
   canvas: HTMLCanvasElement = document.createElement('canvas')
   ctx: CanvasRenderingContext2D = this.canvas.getContext('2d')!
-  constructor() {
+  constructor(width?: number, height?: number) {
+    if (width)
+      this.canvas.width = width
+    if (height)
+      this.canvas.height = height
     return this
   }
 }
