af5b8b2d97528ac012e1dfef0a1afba77cea86ee
Simon he <13917107460@163.com>
2022-09-25 21:39:04 +0800

    feat: 增强arrayToExcel功能

--- src/arrayToExcel.ts
+++ src/arrayToExcel.ts
@@ -1,7 +1,74 @@
-export function arrayToExcel(data: any[], name: string) {
-  const a = document.createElement('a')
-  const blob = new Blob([data.map(v => Object.values(v).join('\t')).join('\n')], { type: 'application/vnd.ms-excel' })
-  a.href = URL.createObjectURL(blob)
-  a.download = name
-  a.click()
+import { createElement } from './createElement'
+import { isStr } from './isStr'
+import { uuid } from './uuid'
+
+interface Target {
+  style?: string
+  background?: string
+  color?: string
+  width?: number
+  height?: number
+}
+
+interface IArrayToExcel {
+  data: Record<string, any>[]
+  filename?: string
+  title?: string[]
+  filter?: string[]
+}
+
+export function arrayToExcel(options: IArrayToExcel) {
+  const { data, filename = uuid(), title, filter } = options
+  if (!data)
+    return
+  const arrData = typeof data != 'object' ? JSON.parse(data) : data
+  let excel = `<table><tr>${title
+    ? title.map(item => `<th align='center'>${item}</th>`).join('')
+    : Object.keys(arrData[0]).map((key) => {
+      if (filter && filter.includes(key))
+        return ''
+      return `<th align='center'>${key}</th>`
+    }).join('')}</tr>`
+  for (let i = 0; i < arrData.length; i++) {
+    let row = '<tr>'
+    for (const index in arrData[i]) {
+      if (filter) {
+        if (!filter.includes(index)) {
+          const target = arrData[i][index] == null ? '' : arrData[i][index]
+          if (isStr(target)) { row += `<td>${target}</td>` }
+          else {
+            const { value, width, colspan = 0, rowspan = 0 } = target
+            row += `<td width=${width} colspan=${colspan} rowspan=${rowspan} style="${generateStyle(target)}">${value}</td>`
+          }
+        }
+      }
+      else {
+        const target = arrData[i][index] == null ? '' : arrData[i][index]
+        if (isStr(target)) { row += `<td align='center'>${target}</td>` }
+        else {
+          const { value, width, colspan = 0, rowspan = 0 } = target
+          row += `<td align='center' width=${width} colspan=${colspan} rowspan=${rowspan} style="${generateStyle(target)}">${value}</td>`
+        }
+      }
+    }
+
+    excel += `${row}</tr>`
+  }
+  excel += '</table>'
+  createElement('a', { href: `data:application/vnd.ms-excel;charset=utf-8,${encodeURIComponent(generateHtml(excel))}`, style: 'visibility:hidden', download: `${filename}.xls` }).click()
+}
+
+function generateStyle(target: Target) {
+  let result = ''
+  const { style = '', background, color } = target
+  if (background)
+    result += `background:${background};`
+  if (color)
+    result += `color:${color};`
+  return result += style
+}
+
+function generateHtml(excel: string) {
+  return `<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:x='urn:schemas-microsoft-com:office:excel' xmlns='http://www.w3.org/TR/REC-html40'><meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8">
+  <meta http-equiv="content-type" content="application/vnd.ms-excel';charset=UTF-8"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body>${excel}</body></html>`
 }
