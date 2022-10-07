3b96f9faf35a8d354421db32ab7ce1f3bf61cb50
Simon-He95 <674949287@qq.com>
2022-10-07 18:18:08 +0800

    chore: update

--- src/useMouse.ts
+++ src/useMouse.ts
@@ -1,5 +1,11 @@
 import { addEventListener } from './addEventListener'
 
-export function useMouse(callback: (e: MouseEvent) => void): () => void {
-  return addEventListener(window, 'mousemove', callback)
+export function useMouse(callback: (e: MouseEvent) => void, delay = 0): () => void {
+  let timeStart = Date.now()
+  return addEventListener(window, 'mousemove', (e) => {
+    if (Date.now() - timeStart >= delay) {
+      timeStart = Date.now()
+      callback(e)
+    }
+  })
 }
