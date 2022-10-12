b448a7590ee00d2014a024d6d87378850714e174
Simon he <13917107460@163.com>
2022-10-12 09:47:28 +0800

    feat: add diff function

---
+++ src/diff.ts
@@ -0,0 +1,20 @@
+interface Options {
+  compare: 'same' | 'different';
+  result: 'value' | 'index';
+}
+
+export function diff(array1: any[], array2: any[], options: Options = { compare: 'same', result: 'value' }) {
+  const same = array1.filter(item => array2.includes(item))
+  const diff = array1.filter(item => !array2.includes(item)).concat(array2.filter(item => !array1.includes(item)))
+  const { compare, result } = options
+  console.log(diff)
+  if (compare === 'same') {
+    return result === 'value'
+      ? same
+      : same.map(item => array1.indexOf(item))
+
+  }
+  return result === 'value'
+    ? diff
+    : diff.map(item => array1.indexOf(item))
+}
