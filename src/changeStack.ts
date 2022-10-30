876be59ac6d594ca616d6241224632630a9c786d
Simon He <13917107469@163.com>
2022-10-30 21:25:02 +0800

    chore: update

--- src/changeStack.ts
+++ src/changeStack.ts
@@ -1,5 +1,5 @@
 import { useMutationObserver } from './useMutationObserver'
-export function changeStack(target: string | Element) {
+export function changeStack(target: string | Element) { // 监听目标元素的子元素变化的栈
   return useMutationObserver(target, () => {
     console.groupCollapsed('childList or sub tree or attributes changed')
     console.trace()
--- src/getPkg.ts
+++ src/getPkg.ts
@@ -1,7 +1,17 @@
 import fs from 'fs'
 import { isFile } from './isFile'
 import { getResolvedPath } from './getResolvedPath'
-export async function getPkg(url = './package.json') {
+interface IPackage {
+  name: string
+  version: string
+  description?: string
+  main?: string
+  module?: string
+  types?: string
+  files?: string[]
+  scripts?: Record<string, string>
+}
+export async function getPkg(url = './package.json'): Promise<IPackage & Record<string, any>> {
   const resolvedPath = getResolvedPath(url)
   if (!isFile(resolvedPath))
     throw new Error(`${resolvedPath} is not a file`)
