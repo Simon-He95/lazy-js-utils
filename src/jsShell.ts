ef3231374f54b21b90ea2926d033f67c843c8800
Simon He <13917107469@163.com>
2022-11-04 12:37:44 +0800

    chore: update

--- src/jsShell.ts
+++ src/jsShell.ts
@@ -1,10 +1,14 @@
 import child_process from 'child_process'
 import { isArray } from './isArray'
-export function jsShell(commander: string | string[], errorExit?: boolean) {
-  return isArray(commander)
+interface IShellMessage {
+  status: number
+  result: string
+}
+export function jsShell<T extends string | string[]>(commander: T, errorExit?: boolean) {
+  return (isArray(commander)
     ? commander.map(command => executor(command))
-    : executor(commander)
-  function executor(commander: string) {
+    : executor(commander)) as T extends string ? IShellMessage : IShellMessage[]
+  function executor(commander: string): IShellMessage {
     const { status, output } = child_process.spawnSync(commander, {
       shell: true,
       stdio: ['inherit', 'pipe', 'inherit'],
@@ -19,7 +23,7 @@ export function jsShell(commander: string | string[], errorExit?: boolean) {
       console.log(result)
       errorExit && process.exit(1)
     }
-    return { status, result }
+    return { status, result } as IShellMessage
   }
 }
 
