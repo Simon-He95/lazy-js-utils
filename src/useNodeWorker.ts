6229a3c88c6bfa210eeac47d3e578fcd6d240f96
Simon He <13917107469@163.com>
2022-11-10 12:06:16 +0800

    chore: update

--- src/useNodeWorker.ts
+++ src/useNodeWorker.ts
@@ -1,12 +1,7 @@
 import { Worker, parentPort } from 'worker_threads'
-import { fileURLToPath } from 'url'
-import path from 'path'
 
-export function useNodeWorker(relativePath: string, data: any) {
+export function useNodeWorker(url: string, data: any) {
   return new Promise((resolve) => {
-    const __filename = fileURLToPath(import.meta.url)
-    const __dirname = path.dirname(__filename)
-    const url = path.resolve(__dirname, relativePath)
     const seprateThread = new Worker(url)
     seprateThread.on('message', resolve)
     seprateThread.postMessage(data)
