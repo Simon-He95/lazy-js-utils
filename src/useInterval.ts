2b4c40016c609c6ce149a0ccd8e4675f7444a7e6
Simon He <13917107469@163.com>
2022-11-14 11:18:31 +0800

    feat(functions): add useTimeout useInterval

---
+++ src/useInterval.ts
@@ -0,0 +1,11 @@
+import { isFn } from './isFn'
+
+type UseIntervalReturn<T> = T extends Function ? (() => void) : undefined
+
+export function useInterval<T>(fn: T, duration: number): UseIntervalReturn<T> {
+  if (!isFn(fn))
+    return undefined as any
+  const timer = setInterval(fn, duration)
+  return (() => clearInterval(timer)) as any
+}
+
