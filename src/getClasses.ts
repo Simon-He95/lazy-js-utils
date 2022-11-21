5365d7f2a415693b445863f1e274374a59d2645d
Simon He <13917107469@163.com>
2022-11-21 16:34:54 +0800

    chore: update

---
+++ src/getClasses.ts
@@ -0,0 +1,30 @@
+export const classesReg = /(<.* class=["']([\w\-\_$ ]*)["'_\-\w$=:\/ ]*\>)/gm
+export function getClasses(str: string, callback: (classes: string, block: string, index: number) => string) {
+  let index = 0
+  return str.replace(classesReg, (match, block, classes) => match.replace(classes, callback(classes, block, index++)))
+}
+
+// const str = `
+// <script setup lang="ts">
+// const props = defineProps<{ modelValue: boolean }>()
+// </script>
+
+// <template>
+//   <div class="red yellow my-class my_class my$c class1" style="background: yellow" >asdas</div>
+//   <div class="red yellow my-class my_class my$c class1" style="background: yellow" />
+// </template>
+
+// <style scoped>
+// .red {
+//   color: red;
+//   font-size: 14px;
+// }
+// .red .a {
+//   color: yellow;
+// }
+// </style>
+
+// `
+// getClasses(str, (classes, block, i) => {
+//   return classes + 'nihao'
+// })
---
+++ src/getStyles.ts
@@ -0,0 +1,32 @@
+export const stylesReg = /(<.* style=['"]([ \w$\-\_:;\n]*)['"][\/ "'$\-_\w]*>)/gm
+export function getStyles(str: string, callback: (style: string, block: string, index: number) => string) {
+  let index = 0
+  return str.replace(stylesReg, (match, block, style) =>
+    match.replace(style, callback(style, block, index++)),
+  )
+}
+
+// const str = `
+// <script setup lang="ts">
+// const props = defineProps<{ modelValue: boolean }>()
+// </script>
+
+// <template>
+//   <div class="red yellow my-class my_class my$c class1" style="background: yellow;color:red;" >asdas</div>
+//   <div class="red yellow my-class my_class my$c class1" style="background: yellow" />
+// </template>
+
+// <style scoped>
+// .red {
+//   color: red;
+//   font-size: 14px;
+// }
+// .red .a {
+//   color: yellow;
+// }
+// </style>
+// `
+// console.log(getStyles(str, (style, block, index) => {
+//   console.log(style)
+//   return style + 'nihao'
+// }))
