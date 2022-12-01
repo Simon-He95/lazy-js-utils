4cd87985485b44e92c2670d3978cb2c02696854d
Simon He <13917107469@163.com>
2022-12-01 14:25:57 +0800

    feat(functions): add isGO isRust isPkg

---
+++ src/isRust.ts
@@ -0,0 +1,9 @@
+import path from 'path'
+import { jsShell } from './jsShell'
+
+export function isRust() {
+  const rootPath = process.cwd()
+  const url = path.resolve(rootPath, 'Cargo.toml')
+  const { result } = jsShell(`test -f "${url}" && echo "0"|| echo "1"`, 'pipe')
+  return result === '0'
+}
