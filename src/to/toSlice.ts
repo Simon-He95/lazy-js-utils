31e90a85721a10ac91470c719a8eb96c160e8973
Simon He <13917107469@163.com>
2022-12-30 13:07:20 +0800

    chore: add toSlice

---
+++ src/to/toSlice.ts
@@ -0,0 +1,12 @@
+export function toSlice(list: any, start?: number): Array<any> {
+  start = start || 0
+  let i = list.length - start
+  const ret: Array<any> = new Array(i)
+  while (i--) ret[i] = list[i + start]
+
+  return ret
+}
+
+// const arr = [1, 3, 5, 7, 9]
+// const data = toArray(arr, 2) // [5,7,9]
+// console.log(data)
