b35f3d679d06a05a68cf4484473a433801fa08fb
Simon He <13917107469@163.com>
2022-12-30 13:07:07 +0800

    chore: add toArray

--- src/to/toArray.ts
+++ src/to/toArray.ts
@@ -1,12 +1,4 @@
-export function toArray(list: any, start?: number): Array<any> {
-  start = start || 0
-  let i = list.length - start
-  const ret: Array<any> = new Array(i)
-  while (i--) ret[i] = list[i + start]
-
-  return ret
+import { isArray } from './../is/isArray'
+export function toArray(array: any) {
+  return isArray(array) ? array : [array]
 }
-
-// const arr = [1, 3, 5, 7, 9]
-// const data = toArray(arr, 2) // [5,7,9]
-// console.log(data)
