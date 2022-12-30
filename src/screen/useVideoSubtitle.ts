bb8a1e8bae024b33206b2cabc7a299d77c0c4d24
Simon He <13917107469@163.com>
2022-12-30 13:07:47 +0800

    feat(functions): add useVideoSubtitle to generate video subtitle

---
+++ src/screen/useVideoSubtitle.ts
@@ -0,0 +1,49 @@
+import fsp from 'fs/promises'
+import { toArray } from '../to/toArray'
+
+interface UseVideoSubtitle {
+  start: number
+  duration?: number
+  end?: number
+  left: number
+  top: number
+  content: string
+}
+
+// 生成弹幕内容
+export function useVideoSubtitle(
+  options: UseVideoSubtitle[],
+  name = 'zh',
+) {
+  const result = toArray(options).map(getTitle).join('\n')
+  generateVtt(result, name)
+}
+
+function getTitle(options: UseVideoSubtitle) {
+  const { start, end, left, top, content, duration } = options
+  const startTime = getTime(start)
+  const endTime = getTime(end || start + (duration ?? 0))
+  const result = `${startTime} --> ${endTime} line:${left}% position:${top}% ${content}`
+  return result
+}
+
+function getTime(t: number) {
+  const _ss = Math.floor(t / 1000)
+  const ttt = addZero(t % 1000, 3) // ms
+  const _mm = Math.floor(_ss / 60) // mm
+  const ss = addZero(_ss % 60, 2)
+  const hh = addZero(Math.floor(_mm / 60), 2)
+  const mm = addZero(_mm % 60, 2)
+  return `${hh}:${mm}:${ss}.${ttt}`
+}
+
+function addZero(n: number, len: number) {
+  if (String(n).length === len)
+    return n
+  const l = len - String(n).length
+  return '0'.repeat(l) + n
+}
+
+function generateVtt(content: string, name: string) {
+  fsp.writeFile(`${name}.vtt`, content, 'utf-8')
+}
