4b4f921f596ccdc8b84fc0bc704e8b96dd2e376c
Simon He <13917107469@163.com>
2023-01-06 12:16:20 +0800

    feat(functions): add singleModel

---
+++ src/js/singleModel.ts
@@ -0,0 +1,21 @@
+export function singleModel(className: any) {
+  let ins: any
+  return new Proxy(className, {
+    construct(Target, args) {
+      if (!ins)
+        ins = new Target(...args)
+
+      return ins
+    },
+  })
+}
+
+// class Video {
+//   constructor() {
+//     console.log('video created');
+//   }
+// }
+// const vi = singleModel(Video)
+// const v1 = new vi()
+// const v2 = new vi()
+// console.log(v1 === v2);
