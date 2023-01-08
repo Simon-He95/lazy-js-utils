650d020a6f4069c427df994a3c365ccf82361929
Simon He <13917107469@163.com>
2023-01-08 23:19:34 +0800

    chore: update

--- src/log/log.ts
+++ src/log/log.ts
@@ -2,7 +2,7 @@ import { isStr } from '../is/isStr'
 import { stringify } from '../object/stringify'
 export function log(
   s: string | number,
-  styleObj: Record<string, string | number> | string,
+  styleObj: Record<string, string | number> | string = {},
   type: 'warn' | 'error' | 'log' = 'log',
 ): void {
   console[type](
