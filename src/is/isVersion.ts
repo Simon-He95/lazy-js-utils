24569bb8109c95abe6f1de8e2a35ebe0b4055771
Simon He <13917107469@163.com>
2023-01-09 09:01:03 +0800

    fix: isVersion

--- src/is/isVersion.ts
+++ src/is/isVersion.ts
@@ -1,4 +1,6 @@
-const NUM = /[0-9]+$/
+const NUM = /^[0-9]+$/
 export function isVersion(version: string) {
   return version.split('.').every(v => NUM.test(v))
 }
+
+// console.log(isVersion('0.0.1'));
