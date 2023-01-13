46b4efe030f775e13114fe5edf231160a342324c
Simon He <13917107469@163.com>
2023-01-13 12:10:11 +0800

    feat(functions): useWebWorker

---
+++ src/js/useWebWorker.ts
@@ -0,0 +1,49 @@
+import { isStr } from '../is/isStr'
+
+export function useWebWorker(url: string): any
+export function useWebWorker(useWebWorkerThread: () => void): any
+export function useWebWorker(url: string | (() => void)) {
+  const worker = new Worker(isStr(url) ? url : fn2workerURL(url))
+  let onMessage: any
+  let errorMessage: any
+  const on = (callback: (data: any) => void) => (onMessage = callback)
+  const error = (err: (err: any) => void) => (errorMessage = err)
+  worker.onerror = error => errorMessage?.(error)
+  worker.onmessage = event => onMessage?.(event.data)
+  return {
+    emit: (msg: any) => worker.postMessage(msg),
+    on,
+    error,
+  }
+}
+
+function fn2workerURL(fn: Function) {
+  const blob = new Blob([`(${fn.toString()})()`], { type: 'text/javascript' })
+  return URL.createObjectURL(blob)
+}
+
+// const { emit, on, error } = useWebWorker(() => {
+//   self.onmessage = (e) => {
+//     const userNum = Number(e.data)
+
+//     fibonacci(userNum)
+//   }
+
+//   function fibonacci(num: number) {
+//     let a = 1
+//     let b = 0
+//     while (num >= 0) {
+//       [a, b] = [a + b, a]
+//       num--
+//     }
+
+//     self.postMessage('nihao')
+//   }
+// })
+// on((data) => {
+//   console.log('on...', data)
+// })
+// emit('hi')
+// error((err) => {
+//   console.log(err)
+// })
