46014e3a0f1fdfa7b95dba3bb4519a015e6eae89
Simon He <13917107469@163.com>
2023-01-31 15:53:09 +0800

    fix: speech type

--- src/speak/speech.ts
+++ src/speak/speech.ts
@@ -20,7 +20,8 @@ interface SpeechResult {
   cancel: () => void
   pause: () => void
   resume: () => void
-  speak: (options: string | SpeechOptions | undefined) => void
+  speak: (options?: string | SpeechOptions) => void
+  isSpeaking: () => boolean
 }
 
 let speechInstance: SpeechSynthesisUtterance
@@ -29,28 +30,27 @@ const defaultOptions = {
   lang: 'zh-CN',
   volume: 1,
   rate: 1,
-  pitch: 1,
+  pitch: 0.5,
   voice: null,
 }
-export function speech(text: string): SpeechResult
-export function speech(options: SpeechOptions): SpeechResult
-export function speech(
-  options: SpeechOptions | string,
-): SpeechResult | undefined {
+export function speech(text?: string): SpeechResult
+export function speech(options?: SpeechOptions): SpeechResult
+export function speech(options?: SpeechOptions | string): SpeechResult {
   const preText = getSpeechInstance(options)
   return {
     cancel: () => speechSynthesis.cancel(),
     pause: () => speechSynthesis.pause(),
     resume: () => speechSynthesis.resume(),
-    speak: (options: string | SpeechOptions | undefined) => {
+    speak: (options?: string | SpeechOptions) => {
       options = options || preText
       getSpeechInstance(options)
       speechSynthesis.speak(speechInstance)
     },
+    isSpeaking: () => speechSynthesis.speaking,
   }
 }
 
-function getSpeechInstance(options: string | SpeechOptions): string {
+function getSpeechInstance(options?: string | SpeechOptions): string {
   speechInstance = Object.assign(
     speechInstance || new SpeechSynthesisUtterance(),
     isStr(options) ? Object.assign(defaultOptions, { text: options }) : options,
