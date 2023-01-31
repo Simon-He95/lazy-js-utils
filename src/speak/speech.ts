a318c637b473e02368ab27904b1d3df892748767
Simon He <13917107469@163.com>
2023-01-31 11:22:05 +0800

    fix(speech): defaultOptions

--- src/speak/speech.ts
+++ src/speak/speech.ts
@@ -24,17 +24,18 @@ interface SpeechResult {
 }
 
 let speechInstance: SpeechSynthesisUtterance
+const defaultOptions = {
+  text: '',
+  lang: 'zh-CN',
+  volume: 1,
+  rate: 1,
+  pitch: 1,
+  voice: null,
+}
 export function speech(text: string): SpeechResult
 export function speech(options: SpeechOptions): SpeechResult
 export function speech(
-  options: SpeechOptions | string = {
-    text: '',
-    lang: 'zh-CN',
-    volume: 1,
-    rate: 1,
-    pitch: 1,
-    voice: null,
-  },
+  options: SpeechOptions | string,
 ): SpeechResult | undefined {
   const preText = getSpeechInstance(options)
   return {
@@ -52,7 +53,7 @@ export function speech(
 function getSpeechInstance(options: string | SpeechOptions): string {
   speechInstance = Object.assign(
     speechInstance || new SpeechSynthesisUtterance(),
-    isStr(options) ? { text: options } : options,
+    isStr(options) ? Object.assign(defaultOptions, { text: options }) : options,
   )
   return speechInstance.text
 }
