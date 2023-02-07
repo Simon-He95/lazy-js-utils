a2c15d0dbdf0ead129cbaeb6044914ed8512ca33
Simon He <13917107469@163.com>
2023-02-07 18:06:23 +0800

    feat(functions): add parseLrc & parseTime

---
+++ src/string/parseLrc.ts
@@ -0,0 +1,25 @@
+import { parseTime } from './parseTime'
+/**
+ * 解析歌词字符串
+ * 得到一个歌词对象的数组
+ * 每个歌词对象
+ * {time: 开始时间, words: 歌词内容}
+ * @param lrc
+ */
+
+interface ParseLrcResult {
+  time: number
+  words: string
+}
+export function parseLrc(lrc: string) {
+  const lines = lrc.split('\n')
+  return lines.reduce((result, str) => {
+    const parts = str.split(']')
+    const timeStr = parts[0].slice(1)
+    result.push({
+      time: parseTime(timeStr),
+      words: parts[1],
+    })
+    return result
+  }, [] as ParseLrcResult[])
+}
---
+++ src/string/parseTime.ts
@@ -0,0 +1,13 @@
+/**
+ * 将时间字符串解析为数字 (秒)
+ * '01:02:03' => 1 * 60 * 60 + 2 * 60 + 3
+ * @param { string } timeStr 字符串
+ * @returns
+ */
+export function parseTime(timeStr: string) {
+  const parts = timeStr.split(':')
+  const len = parts.length - 1
+  return parts.reduce((pre, cur, i) => pre + +cur * 60 ** (len - i), 0)
+}
+
+// console.log(parseTime('01:02:03'))
