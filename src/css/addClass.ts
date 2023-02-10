0844c61ad3235e40967e863d6669cab72d959efe
Simon He <13917107469@163.com>
2023-02-10 10:51:25 +0800

    chore: add comment

--- src/css/addClass.ts
+++ src/css/addClass.ts
@@ -1,6 +1,12 @@
 import { mount } from '../utils'
 import type { MaybeElement } from './../types'
 
+/**
+ * dom上添加class
+ * @param { MaybeElement } selector 元素
+ * @param { string } className class类
+ * @returns
+ */
 export function addClass(selector: MaybeElement, className: string) {
   return mount(selector, el => el.classList.add(className))
 }
--- src/css/removeClass.ts
+++ src/css/removeClass.ts
@@ -1,6 +1,12 @@
 import { mount } from '../utils'
 import type { MaybeElement } from './../types'
 
+/**
+ * dom上删除class
+ * @param { MaybeElement } selector 元素
+ * @param { string } className class类
+ * @returns
+ */
 export function removeClass(selector: MaybeElement, className: string) {
   return mount(selector, el => el.classList.remove(className))
 }
