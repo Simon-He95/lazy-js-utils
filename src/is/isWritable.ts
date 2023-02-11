e75e733f911442de29180ae77f8746174ac7d6b9
Simon He <13917107469@163.com>
2023-02-12 01:14:28 +0800

    feat: add isWritiable

---
+++ src/is/isWritable.ts
@@ -0,0 +1,16 @@
+import fs from 'fs'
+
+/**
+ * 同步地测试用户对 path 指定的文件或目录的权限
+ * @param { string } filename 文件或目录路径
+ * @returns
+ */
+export function isWritable(filename: string): boolean {
+  try {
+    fs.accessSync(filename, fs.constants.W_OK)
+    return true
+  }
+  catch {
+    return false
+  }
+}
