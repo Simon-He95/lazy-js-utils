30a9dfdbe58fa1d9cac54170a6771515be935392
Simon He <13917107469@163.com>
2023-02-14 13:00:22 +0800

    feat(functions): add StyleScoped

---
+++ src/script/addStyleScoped.ts
@@ -0,0 +1,5 @@
+export function addStyleScoped(s: string, scoped: string) {
+  return s.replace(/([\.#]*\w+)[\s.>+\w]*{/gm, (v, k) =>
+    v.replace(k, `[${scoped}] ${k}`),
+  )
+}
--- src/script/index.ts
+++ src/script/index.ts
@@ -1,3 +1,4 @@
 export * from './addLink'
 export * from './addScript'
 export * from './addStyle'
+export * from './addStyleScoped'
