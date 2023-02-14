d611adccecc565908d99840728a6ebad53342a75
Simon He <13917107469@163.com>
2023-02-14 13:00:05 +0800

    feat: addStyle support styleObj

--- src/script/addStyle.ts
+++ src/script/addStyle.ts
@@ -1,37 +1,44 @@
-import fsp from 'fs/promises'
-import path from 'path'
-import process from 'process'
-import { isNm } from '../is/isNm'
-import { isRelative } from '../is/isRelative'
 import { createElement } from '../event/createElement'
 import { removeElement } from '../event/removeElement'
 import { insertElement } from '../event/insertElement'
-
+import { isStr } from '../is/isStr'
+import { hyphenate } from '../string'
+import { addStyleScoped } from './addStyleScoped'
 /**
  * 将style添加到head
  * @param { string } s style
+ * @param { string } scoped 独立作用域 针对局部组件生效<div data-v-xxx></div>, 传入data-v-xxx
  * @returns
  */
-export async function addStyle(s: string): Promise<() => void> {
-  try {
-    const style = createElement(
-      'style',
-      {
-        type: 'text/css',
-      },
-      isNm(s)
-        ? await fsp.readFile(
-          path.resolve(process.cwd(), 'node_modules', s),
-          'utf8',
-        )
-        : isRelative(s)
-          ? await fsp.readFile(path.resolve(process.cwd(), s), 'utf8')
-          : s,
-    )
-    insertElement(document.head, style, null)
-    return () => removeElement(style)
-  }
-  catch (error: any) {
-    throw new Error(error)
-  }
+type StyleObject = Record<string, Record<string, string | number>>
+export async function addStyle(
+  s: string | StyleObject,
+  scoped?: string,
+): Promise<() => void> {
+  const style = !isStr(s)
+    ? Object.keys(s).reduce((result, key) => {
+      const obj = s[key]
+      return (result += `${key} { ${Object.keys(obj).reduce(
+          (r, k: any) => (r += `${hyphenate(k)}: ${obj[k]};`),
+          '',
+        )} }`)
+    }, '')
+    : s
+
+  const styleEl = createElement(
+    'style',
+    {
+      type: 'text/css',
+    },
+    scoped ? addStyleScoped(style, scoped) : style,
+  )
+  insertElement(document.head, styleEl, null)
+  return () => removeElement(style)
 }
+
+// addStyle({
+//   '#main': {
+//     backgroundColor: 'red',
+//     fontSize: '20px',
+//   },
+// }, 'data-v-xxx')
