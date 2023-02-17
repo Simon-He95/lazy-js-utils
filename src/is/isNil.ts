13450e71d366c1d7fab86a5d9363f6b2468f3910
Simon He <13917107469@163.com>
2023-02-17 10:52:56 +0800

    chore: enhance isNill type

--- src/is/isNil.ts
+++ src/is/isNil.ts
@@ -1,6 +1,6 @@
 /**
  * 判断是否是null或undefined
  */
-export function isNil(value: any): boolean {
+export function isNil(value: any): value is null | undefined {
   return value === null || value === undefined
 }
