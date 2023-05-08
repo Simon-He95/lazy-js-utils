a76933d70502e22948f0d43ebeaa50da3904af89
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-05-09 00:44:53 +0800

    refactor(collisionDetection): getBoundingClientRect

--- src/event/collisionDetection.ts
+++ src/event/collisionDetection.ts
@@ -9,24 +9,21 @@ import { findElement } from './findElement'
  * @returns
  */
 export function collisionDetection(o1: MaybeElement, o2: MaybeElement) {
-  const obj1: HTMLElement = isStr(o1) ? findElement(o1)! : o1
-  const obj2: HTMLElement = isStr(o2) ? findElement(o2)! : o2
+  const obj1: HTMLElement | null = isStr(o1) ? findElement(o1) : o1
+  const obj2: HTMLElement | null = isStr(o2) ? findElement(o2) : o2
 
   if (!obj1 || !obj2)
     return false
-  const left1_start = obj1.offsetLeft
-  const left1_end = obj1.offsetLeft + obj1.offsetWidth
-  const left2_start = obj2.offsetLeft
-  const left2_end = obj2.offsetLeft + obj2.offsetWidth
-  const top1_start = obj1.offsetTop
-  const top1_end = obj1.offsetTop + obj1.offsetHeight
-  const top2_start = obj2.offsetTop
-  const top2_end = obj2.offsetTop + obj2.offsetHeight
-  // 判断是否碰撞
-  return !(
-    left1_start > left2_end
-    || left1_end < left2_start
-    || top1_start > top2_end
-    || top1_end < top2_start
+  const rect1 = obj1.getBoundingClientRect()
+  const rect2 = obj2.getBoundingClientRect()
+  const overlapX = Math.max(
+    0,
+    Math.min(rect1.right, rect2.right) - Math.max(rect1.left, rect2.left),
   )
+  const overlapY = Math.max(
+    0,
+    Math.min(rect1.bottom, rect2.bottom) - Math.max(rect1.top, rect2.top),
+  )
+
+  return overlapX > 0 && overlapY > 0
 }
