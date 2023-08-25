18eeaa51bb4260001cc550ff4b5581845e6b4b62
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-08-26 00:11:51 +0800

    fix(throttle): multiple params

--- src/perf/throttle.ts
+++ src/perf/throttle.ts
@@ -6,10 +6,10 @@
  */
 export function throttle(fn: Function, stop: number) {
   let start = 0
-  return function (this: any, e?: any) {
+  return function (this: any, ...args: any[]) {
     const end = Date.now()
     if (end - start >= stop) {
-      const result = fn.call(this, e)
+      const result = fn.call(this, ...args)
       start = end
       return result
     }
