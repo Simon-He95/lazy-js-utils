62cc176a6cab6381dae7f93fe22c9ee886e58205
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-10-27 09:34:31 +0800

    chore(useJSONParse): 处理Date类型parse不了用正则处理

--- src/string/useJSONParse.ts
+++ src/string/useJSONParse.ts
@@ -9,16 +9,28 @@ const moreCommaReg = /,(\s*})/gm
  * @returns
  */
 export function useJSONParse(str: string) {
-  return JSON.parse(
-    str
-      .replace(keyReg, (match, key) => match.replace(key, `"${key}"`))
-      .replace(valueReg, ': "$1"')
-      .replace(commaLackReg, (match, value) =>
-        match.replace(value, `${value},`),
-      )
-      .replace(commaMoreReg, (match) => match.replace(',', ''))
-      .replace(moreCommaReg, (_, v) => v),
-  )
+  try {
+    return JSON.parse(
+      str
+        .replace(keyReg, (match, key) => match.replace(key, `"${key}"`))
+        .replace(valueReg, ': "$1"')
+        .replace(commaLackReg, (match, value) =>
+          match.replace(value, `${value},`),
+        )
+        .replace(commaMoreReg, (match) => match.replace(',', ''))
+        .replace(moreCommaReg, (_, v) => v),
+    )
+  } catch (_) {
+    return str
+      .trim()
+      .replace(/\n+/g, '\n')
+      .split('\n')
+      .reduce((result, item: string) => {
+        const [key, val] = item.split(':') as string[]
+        result[key.trim()] = val.trim()
+        return result
+      }, {} as any)
+  }
 }
 
 // const data = `{
