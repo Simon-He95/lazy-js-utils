38db5a530b36cf9aed4a58de1ee16c2adc8a2d96
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-11-08 23:03:55 +0800

    feat(object): add objectToMap & mapToObject

---
+++ src/object/mapToObject.ts
@@ -0,0 +1,17 @@
+import { isObject } from '../is'
+
+export function mapToObject(map: Map<any, any>) {
+  return Array.from(map).reduce((result, [key, value]) => {
+    if (isObject(key)) {
+      key = JSON.stringify(key)
+    }
+    result[key] = value
+    return result
+  }, {} as Record<string, any>)
+}
+
+// const map = new Map()
+// map.set(1, '123')
+// map.set({ name: 'ss' }, '2')
+
+// console.log(mapToObject(map))
---
+++ src/object/objectToMap.ts
@@ -0,0 +1,8 @@
+export function objectToMap(obj: Record<string, any>) {
+  return Object.keys(obj).reduce((result, key) => {
+    const value = obj[key]
+    if (/{.*}/.test(key)) result.set(JSON.parse(key), value)
+    else result.set(key, value)
+    return result
+  }, new Map())
+}
