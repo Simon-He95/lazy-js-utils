66fbe3d80bceb9d799917d3c7206c24f38cb6a8e
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-11-30 16:06:04 +0800

    fix(useJSONParse): ,\n

--- src/string/useJSONParse.ts
+++ src/string/useJSONParse.ts
@@ -21,7 +21,10 @@ export function useJSONParse(str: string) {
         .replace(moreCommaReg, (_, v) => v),
     )
   } catch (_) {
-    str = str.trim().replace(/(\/\*[\s\S]*?\*\/|\/\/.*)/g, '')
+    str = str
+      .trim()
+      .replace(/(\/\*[\s\S]*?\*\/|\/\/.*)/g, '')
+      .replace(/,\n\s*}/g, '\n }')
     if (str.endsWith(';')) str = str.slice(0, -1)
     return str
       .slice(1, -1)
@@ -45,66 +48,138 @@ export function useJSONParse(str: string) {
 }
 
 const data = `{
-  defaultFullscreen: { type: Boolean },
-  // Can it be full screen
-  canFullscreen: { type: Boolean, default: true },
-  // After enabling the wrapper, the bottom can be increased in height
-  wrapperFooterOffset: { type: Number, default: 0 },
-  // Warm reminder message
-  helpMessage: [String, Array] as PropType<string | string[]>,
-  // Whether to setting wrapper
-  useWrapper: { type: Boolean, default: true },
-  loading: { type: Boolean },
-  loadingTip: { type: String },
-  /**
-   * @description: Show close button
-   */
-  showCancelBtn: { type: Boolean, default: true },
-  /**
-   * @description: Show confirmation button
-   */
-  showOkBtn: { type: Boolean, default: true },
-
-  wrapperProps: Object as PropType<Partial<ModalWrapperProps>>,
-
-  afterClose: Function as PropType<() => Promise<VueNode>>,
-
-  bodyStyle: Object as PropType<CSSProperties>,
-
-  closable: { type: Boolean, default: true },
-
-  closeIcon: Object as PropType<VueNode>,
-
-  confirmLoading: { type: Boolean },
-
-  destroyOnClose: { type: Boolean },
-
-  footer: Object as PropType<VueNode>,
-
-  getContainer: Function as PropType<() => any>,
-
-  mask: { type: Boolean, default: true },
-
-  maskClosable: { type: Boolean, default: true },
-  keyboard: { type: Boolean, default: true },
-
-  maskStyle: Object as PropType<CSSProperties>,
-
-  okType: { type: String, default: 'primary' },
-
-  okButtonProps: Object as PropType<ButtonProps>,
-
-  cancelButtonProps: Object as PropType<ButtonProps>,
-
-  title: { type: String },
-
-  open: { type: Boolean },
-
-  width: [String, Number] as PropType<string | number>,
-
-  wrapClassName: { type: String },
-
-  zIndex: { type: Number },
-};`
+  clickToRowSelect: { type: Boolean, default: true },
+  isTreeTable: Boolean,
+  tableSetting: propTypes.shape<TableSetting>({}),
+  inset: Boolean,
+  sortFn: {
+    type: Function as PropType<(sortInfo: SorterResult) => any>,
+    default: DEFAULT_SORT_FN,
+  },
+  filterFn: {
+    type: Function as PropType<(data: Partial<Recordable<string[]>>) => any>,
+    default: DEFAULT_FILTER_FN,
+  },
+  showTableSetting: Boolean,
+  autoCreateKey: { type: Boolean, default: true },
+  striped: { type: Boolean, default: true },
+  showSummary: Boolean,
+  summaryFunc: {
+    type: [Function, Array] as PropType<(...arg: any[]) => any[]>,
+    default: null,
+  },
+  summaryData: {
+    type: Array as PropType<Recordable[]>,
+    default: null,
+  },
+  indentSize: propTypes.number.def(24),
+  canColDrag: { type: Boolean, default: true },
+  api: {
+    type: Function as PropType<(...arg: any[]) => Promise<any>>,
+    default: null,
+  },
+  beforeFetch: {
+    type: Function as PropType<Fn>,
+    default: null,
+  },
+  afterFetch: {
+    type: Function as PropType<Fn>,
+    default: null,
+  },
+  handleSearchInfoFn: {
+    type: Function as PropType<Fn>,
+    default: null,
+  },
+  fetchSetting: {
+    type: Object as PropType<FetchSetting>,
+    default: () => {
+      return FETCH_SETTING;
+    },
+  },
+  immediate: { type: Boolean, default: true },
+  emptyDataIsShowTable: { type: Boolean, default: true },
+  // 额外的请求参数
+  searchInfo: {
+    type: Object as PropType<Recordable>,
+    default: null,
+  },
+  // 默认的排序参数
+  defSort: {
+    type: Object as PropType<Recordable>,
+    default: null,
+  },
+  // 使用搜索表单
+  useSearchForm: propTypes.bool,
+  // 表单配置
+  formConfig: {
+    type: Object as PropType<Partial<FormProps>>,
+    default: null,
+  },
+  columns: {
+    type: Array as PropType<BasicColumn[]>,
+    default: () => [],
+  },
+  showIndexColumn: { type: Boolean, default: true },
+  indexColumnProps: {
+    type: Object as PropType<BasicColumn>,
+    default: null,
+  },
+  actionColumn: {
+    type: Object as PropType<BasicColumn>,
+    default: null,
+  },
+  ellipsis: { type: Boolean, default: true },
+  isCanResizeParent: { type: Boolean, default: false },
+  canResize: { type: Boolean, default: true },
+  clearSelectOnPageChange: propTypes.bool,
+  resizeHeightOffset: propTypes.number.def(0),
+  rowSelection: {
+    type: Object as PropType<TableRowSelection | null>,
+    default: null,
+  },
+  title: {
+    type: [String, Function] as PropType<string | ((data: Recordable) => string)>,
+    default: null,
+  },
+  titleHelpMessage: {
+    type: [String, Array] as PropType<string | string[]>,
+  },
+  maxHeight: propTypes.number,
+  dataSource: {
+    type: Array as PropType<Recordable[]>,
+    default: null,
+  },
+  rowKey: {
+    type: [String, Function] as PropType<string | ((record: Recordable) => string)>,
+    default: '',
+  },
+  bordered: propTypes.bool,
+  pagination: {
+    type: [Object, Boolean] as PropType<PaginationProps | boolean>,
+    default: null,
+  },
+  loading: propTypes.bool,
+  rowClassName: {
+    type: Function as PropType<(record: TableCustomRecord<any>, index: number) => string>,
+  },
+  scroll: {
+    type: Object as PropType<{ x: number | string | true; y: number | string }>,
+    default: null,
+  },
+  beforeEditSubmit: {
+    type: Function as PropType<
+      (data: {
+        record: Recordable;
+        index: number;
+        key: string | number;
+        value: any;
+      }) => Promise<any>
+    >,
+  },
+  size: {
+    type: String as PropType<SizeType>,
+    default: DEFAULT_SIZE,
+  },
+}`
 
 console.log(useJSONParse(data)) // { name: 'simon', age: '14' }
