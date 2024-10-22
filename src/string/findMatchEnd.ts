e4ceacba51e3c6736cda66953748ba0f41e2bdc6
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-10-22 10:42:40 +0800

    chore: add jsdoc

--- src/string/findMatchEnd.ts
+++ src/string/findMatchEnd.ts
@@ -6,6 +6,14 @@ const matchMap = {
   '"': '"',
   '<': '>',
 }
+/**
+ * 查找匹配符号的结束偏移量
+ *
+ * @param matchStr - 要匹配的符号
+ * @param code - 包含符号的代码字符串
+ * @returns 匹配符号的结束偏移量
+ * @throws 如果在代码字符串中找不到匹配符号的结束偏移量，则抛出错误
+ */
 export function findMatchEndOffset(matchStr: string, code: string) {
   let offset = 0
   let dep = 0
