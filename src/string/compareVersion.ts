2f5fa17c0949574cd871c69588a5c6548c37b4e7
Simon He <57086651+Simon-He95@users.noreply.github.com>
2025-02-25 14:06:14 +0800

    fix: add JSDoc comments to compareVersion function

--- src/string/compareVersion.ts
+++ src/string/compareVersion.ts
@@ -1,3 +1,10 @@
+/**
+ * 比较两个版本字符串。
+ *
+ * @param {string} v1 - 第一个版本字符串。
+ * @param {string} v2 - 第二个版本字符串。
+ * @returns {number} - 如果 v1 大于 v2 返回 1，如果 v2 大于 v1 返回 -1，如果它们相等返回 0。
+ */
 export function compareVersion(v1: string, v2: string): number {
   const v1Arr = v1.split('.')
   const v2Arr = v2.split('.')
