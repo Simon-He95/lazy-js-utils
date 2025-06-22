fa0529bd448e7cbaa377ccac64004d5948ce5a28
Simon He <57086651+Simon-He95@users.noreply.github.com>
2025-06-22 13:25:43 +0800

    feat: enhance useInterval function to support pause and resume controls

--- src/event/useInterval.ts
+++ src/event/useInterval.ts
@@ -1,16 +1,44 @@
 import { isFn } from '../is/isFn'
 
-type UseIntervalReturn<T> = T extends Function ? () => void : undefined
+interface UseIntervalControls {
+  isActive: () => boolean
+  pause: () => void
+  resume: () => void
+}
+
+type UseIntervalReturn<T> = T extends Function ? UseIntervalControls : undefined
 
 /**
- * setInterval
+ * 可暂停和恢复的 setInterval
  * @param { Function } fn 函数
  * @param { number } duration 间隔时间
- * @returns 停止
+ * @returns {{ isActive: () => boolean; pause: () => void; resume: () => void } | undefined }
  */
 export function useInterval<T>(fn: T, duration: number): UseIntervalReturn<T> {
   if (!isFn(fn))
     return undefined as any
-  const timer = setInterval(fn, duration)
-  return (() => clearInterval(timer)) as any
+
+  let timer: number | null = null
+  let _isActive = false
+
+  const controls: UseIntervalControls = {
+    isActive: () => _isActive,
+    pause: () => {
+      if (timer) {
+        clearInterval(timer)
+        timer = null
+        _isActive = false
+      }
+    },
+    resume: () => {
+      if (timer)
+        return
+      timer = setInterval(fn as Function, duration)
+      _isActive = true
+    },
+  }
+
+  controls.resume()
+
+  return controls as any
 }
